d30bd9ce5489e37305fb76ecf16c2e008b3b9e19
niklas sandberg <niklas.sandberg@gmail.com>
2021-12-07 13:13:55 +0100

    Added .gitignore file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("HelloWorld! Change3");
+	System.out.println("HelloWorld! Changes to main file /Task6");
     }
 }
